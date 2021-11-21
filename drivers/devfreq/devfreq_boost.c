d3092b15bdf0607df560e7202bf3845da4030896
Dakkshesh <dakkshesh5@gmail.com>
2021-11-21 16:49:26 +0000

    drivers: devfreq_boost: Skip boosting if Profile is set to Battery Mode

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -55,15 +55,15 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
-		return;
+  if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
+    return;
 
-	set_bit(INPUT_BOOST, &b->state);
-	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
-		msecs_to_jiffies(CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS))) {
-		/* Set the bit again in case we raced with the unboost worker */
-		set_bit(INPUT_BOOST, &b->state);
-		wake_up(&b->boost_waitq);
+  set_bit(INPUT_BOOST, &b->state);
+  if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
+			msecs_to_jiffies(CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS))) {
+		    /* Set the bit again in case we raced with the unboost worker */
+		    set_bit(INPUT_BOOST, &b->state);
+		    wake_up(&b->boost_waitq);
 	}
 }
 
@@ -79,8 +79,8 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 {
 	unsigned long boost_jiffies, curr_expires, new_expires;
 
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
-		return;
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || active_mode() == 1)
+	  return;
 
 	boost_jiffies = msecs_to_jiffies(duration_ms);
 	do {
