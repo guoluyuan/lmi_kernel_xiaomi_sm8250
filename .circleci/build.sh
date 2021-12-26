163b937ef087dbc0bd4902b6259acf1889932b5e
NotZeetaa <rodrigo2005contente@gmail.com>
2021-12-26 23:33:23 +0000

    ci: Switch to lld
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -54,7 +54,8 @@ function compile() {
                           ARCH=arm64 \
 			  CC=clang \
 			  CROSS_COMPILE=aarch64-linux-gnu- \
-			  CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+			  CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+			  LD=aarch64-elf-ld.lld
 
     if ! [ -a "$IMAGE" ]; then
         finerr
