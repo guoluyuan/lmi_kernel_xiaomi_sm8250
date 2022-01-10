0549ed41897d9ac41c160e3c6b411472ee5daea3
Cyber Knight <cyberknight755@gmail.com>
2022-01-10 21:22:02 +0000

    kernel/power: Reduce verbosity of logging
    Silences:
    [41385.565295] Restarting tasks ...
    [41385.579844] Resume caused by IRQ 178, WLAN_CE_2
    [41385.579855] PM: PM: suspend exit 2022-01-10 10:54:26.808561566 UTC
    [41385.579857] PM: suspend exit
    [41385.680504] PM: PM: suspend entry 2022-01-10 10:54:26.909195420 UTC
    [41385.680519] PM: suspend entry (deep)
    [41385.704201] Freezing user space processes ...
    [41385.716293] Freezing remaining freezable tasks ...

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -331,7 +331,7 @@ static int suspend_test(int level)
 {
 #ifdef CONFIG_PM_DEBUG
 	if (pm_test_level == level) {
-		pr_info("suspend debug: Waiting for %d second(s).\n",
+		pr_debug("suspend debug: Waiting for %d second(s).\n",
 				pm_test_delay);
 		mdelay(pm_test_delay * 1000);
 		return 1;
@@ -514,7 +514,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	if (error) {
 		last_dev = suspend_stats.last_failed_dev + REC_FAILED_NUM - 1;
 		last_dev %= REC_FAILED_NUM;
-		pr_err("Some devices failed to suspend, or early wake event detected\n");
+		pr_debug("Some devices failed to suspend, or early wake event detected\n");
 		log_suspend_abort_reason("%s device failed to suspend, or early wake event detected",
 			suspend_stats.failed_devs[last_dev]);
 		goto Recover_platform;
@@ -633,7 +633,7 @@ int pm_suspend(suspend_state_t state)
 	if (state <= PM_SUSPEND_ON || state >= PM_SUSPEND_MAX)
 		return -EINVAL;
 
-	pr_info("suspend entry (%s)\n", mem_sleep_labels[state]);
+	pr_debug("suspend entry (%s)\n", mem_sleep_labels[state]);
 	error = enter_state(state);
 	if (error) {
 		suspend_stats.fail++;
@@ -641,7 +641,7 @@ int pm_suspend(suspend_state_t state)
 	} else {
 		suspend_stats.success++;
 	}
-	pr_info("suspend exit\n");
+	pr_debug("suspend exit\n");
 	return error;
 }
 EXPORT_SYMBOL(pm_suspend);
