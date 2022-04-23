e8603375320b4afc1632fc8eea5bf031f38fdc46
NotZeetaa <rodrigo2005contente@gmail.com>
2022-04-23 10:38:29 +0000

    build2: Switch to AtomX clang

--- build2.sh
+++ build2.sh
@@ -47,7 +47,7 @@ LINKER=ld.lld
 
 ##----------------------------------------------------------##
 # Specify compiler [ proton, atomx, eva, aosp ]
-COMPILER=prelude
+COMPILER=atomx
 
 ##----------------------------------------------------------##
 # Clone ToolChain
